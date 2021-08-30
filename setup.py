ef25f6f3a0a7baa39e8032f7c53e297963fc7bf3
techpluscraft <techpluscraft@gmail.com>
2021-08-30 09:13:08 -0400

    v0.0.2 Added palindrome function

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ classifiers = [
  
 setup(
   name='techdevutil',
-  version='0.0.1',
+  version='0.0.2',
   description='A util library',
   long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
   url='',  
--- techdevutil/__init__.py
+++ techdevutil/__init__.py
@@ -10,3 +10,10 @@ def multiply_numbers(num1,num2):
 def divide_numbers(num1,num2):
     return num1 / num2
 
+def palindrome(word):
+    wordlength = len(word)
+    reversedword=word[wordlength::-1]
+    if word == reversedword:
+        return(f"{word} is a palindrome")
+    else:
+        return(f"{word} is not a palindrome")
